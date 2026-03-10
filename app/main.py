b3f0543a5401a017adf0e23c665bc0510f351717
Brunaveloso7 <brunavelosoribeiro7@gmail.com>
2026-03-10 16:02:07 -0300

    Finalizando projeto

---
+++ app/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+from app.database import Base, engine
+from app.route.serie import router
+
+Base.metadata.create_all(bind=engine)
+ 
+app = FastAPI ()
+
+app.include_router(router)
+
+@app.get("/")
+async def health_check():
+    return{"status": "API Online"}
